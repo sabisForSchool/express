d4c825cf813b50c5da571ab2ccf2c44b6363730b
sabisForSchool <gataespacial18@gmail.com>
2023-09-27 10:40:21 -0300

    método de atualizar user

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -14,6 +14,11 @@ class usuarioController{
         const id = req.params.id
         res.status(200).send(usuariosCadastrados[id])
     }
+    static atualizarUser(req, res){
+        const id = req.params.id
+        usuariosCadastrados[id] = req.body
+        return res.status(200).send({message:"user atualizado"})
+    }
 }
 //faça uma função pra atualizar 
 export default usuarioController
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,6 @@ const routes = new Router();
 routes.get("/usuarios", usuariosController.listarUsuarios)
 routes.post('/usuario', usuariosController.criarUsuario)
 routes.get("/usuario/:id", usuariosController.listarUserPorId)
+routes.put("/usuario/:id", usuariosController.atualizarUser)
 
 export default routes;
