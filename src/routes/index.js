74be969e880f42b8c3f59b2b44fce05842dfdf50
sabisForSchool <gataespacial18@gmail.com>
2023-09-29 07:24:56 -0300

    rota de delete

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,5 +9,6 @@ routes.get("/usuarios", usuariosController.listarUsuarios)
 routes.post('/usuario', usuariosController.criarUsuario)
 routes.get("/usuario/:id", usuariosController.listarUserPorId)
 routes.put("/usuario/:id", usuariosController.atualizarUser)
+routes.delete("/usuario/:id", usuariosController.deletar)
 
 export default routes;
