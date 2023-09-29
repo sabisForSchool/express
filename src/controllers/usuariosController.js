8fccf948b4419c215b7bc09b38bcefe360001eba
sabisForSchool <gataespacial18@gmail.com>
2023-09-29 08:12:43 -0300

    adequando o métodos das rotas ao uuid

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,35 +1,54 @@
+import {v4 as uuidv4} from "uuid"
 const usuariosCadastrados = []
 
 class usuarioController{
     static criarUsuario(req, res){
         const novoUser = req.body
-        novoUser.id = usuariosCadastrados.length
+        novoUser.id = uuidv4()
         usuariosCadastrados.push(novoUser)
         return res.status(201).send({message:"usuário criado"})
     }
     static listarUsuarios(req, res){
-        res.status(200).send(usuariosCadastrados)
+        if(usuariosCadastrados.length == 0){
+            return res.status(404).send({message:"sem usuários cadastrados"})
+        }
+        return res.status(200).send(usuariosCadastrados)
     }
     static listarUserPorId(req, res){
         const id = req.params.id
-        res.status(200).send(usuariosCadastrados[id])
+        const user = usuariosCadastrados.find((user) => user.id == id)
+        if(user){
+            return res.status(200).send(user)
+        }else{
+            return res.status(404).send({message: "usuário não existe"})
+        }
+        
     }
     static atualizarUser(req, res){
         const id = req.params.id
-        usuariosCadastrados[id] = req.body
-        return res.status(200).send({message:"user atualizado"})
+        let user = usuariosCadastrados.find((user) => user.id == id)
+        if(user){
+            const index = usuariosCadastrados.indexOf(user)
+            usuariosCadastrados[index].name = req.body.name
+            usuariosCadastrados[index].senha = req.body.senha
+            usuariosCadastrados[index].username = req.body.username
+            return res.status(200).send({message:"user atualizado"})
+        }else{
+            return res.status(404).send({message:"usuárionão encontrado"})
+        }
+        
     }
     static deletar(req, res){
         const id = req.params.id
-        if(usuariosCadastrados.length == 0){
-            return res.status(404).send({message:"Não há usuários cadastrados"})
-        }
-        else if(usuariosCadastrados.length >= id && id>=0){
-            usuariosCadastrados.splice(id, 1)
-            return res.status(200).send({message:"usuário deletado"})
+        const user = usuariosCadastrados.find((user) => user.id === id)
+        if(user){
+            const index = usuariosCadastrados.indexOf(user)
+            usuariosCadastrados.splice(index, 1)
+            res.status(200).send({message: "usuário deletado"})
         }else{
-            return res.status(404).send({message:"usuário não existe"})
+            res.status(404).send({message: "usuário não encontrado"})
         }
+        
     }
 }
 export default usuarioController
