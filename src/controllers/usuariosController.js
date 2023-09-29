c8391b378a89abaf6a4deafc0675874b3f04ea7d
sabisForSchool <gataespacial18@gmail.com>
2023-09-29 08:18:54 -0300

    adicionando testes para garantir todos os dados no método atualizar

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -27,12 +27,24 @@ class usuarioController{
     static atualizarUser(req, res){
         const id = req.params.id
         let user = usuariosCadastrados.find((user) => user.id == id)
+
         if(user){
             const index = usuariosCadastrados.indexOf(user)
-            usuariosCadastrados[index].name = req.body.name
-            usuariosCadastrados[index].senha = req.body.senha
-            usuariosCadastrados[index].username = req.body.username
-            return res.status(200).send({message:"user atualizado"})
+            //testes para garantir  ue a requisição tem todos os dados
+            if(!req.body.name){
+                return res.status(500).send({message: "o campo name é obrigatório, caso não queira mudá-lo coloque o valor antigo"})
+            }
+            else if(!req.body.senha){
+                return res.status(500).send({message: "o campo senha é obrigatório, caso não queira mudá-lo coloque o valor antigo"})
+            }
+            else if(!req.body.username){
+                return res.status(500).send({message: "o campo username é obrigatório, caso não queira mudá-lo coloque o valor antigo"})
+            }else{//possui todos os dados
+                usuariosCadastrados[index].name = req.body.name
+                usuariosCadastrados[index].senha = req.body.senha
+                usuariosCadastrados[index].username = req.body.username
+                return res.status(200).send({message:"user atualizado"})
+            }
         }else{
             return res.status(404).send({message:"usuárionão encontrado"})
         }
@@ -44,9 +56,9 @@ class usuarioController{
         if(user){
             const index = usuariosCadastrados.indexOf(user)
             usuariosCadastrados.splice(index, 1)
-            res.status(200).send({message: "usuário deletado"})
+            return res.status(200).send({message: "usuário deletado"})
         }else{
-            res.status(404).send({message: "usuário não encontrado"})
+            return res.status(404).send({message: "usuário não encontrado"})
         }
         
     }
