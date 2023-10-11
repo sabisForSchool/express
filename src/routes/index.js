ce562f817e3f1e75d01d3ec09087f98495180414
sabisForSchool <gataespacial18@gmail.com>
2023-10-11 07:37:55 -0300

    FIX: melhorando identação

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,11 @@ const routes = new Router();
 
 
 
-routes.get("/usuarios", usuariosController.listarUsuarios)
-routes.post('/usuario', usuariosController.criarUsuario)
-routes.get("/usuario/:id", usuariosController.listarUserPorId)
-routes.put("/usuario/:id", usuariosController.atualizarUser)
-routes.delete("/usuario/:id", usuariosController.deletar)
+routes
+    .get("/usuarios", usuariosController.listarUsuarios)
+    .post('/usuario', usuariosController.criarUsuario)
+    .get("/usuario/:id", usuariosController.listarUserPorId)
+    .put("/usuario/:id", usuariosController.atualizarUser)
+    .delete("/usuario/:id", usuariosController.deletar)
 
 export default routes;
