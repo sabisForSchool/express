74a20a44e27269cbe8ea72186c8e39a317aaf930
sabisForSchool <gataespacial18@gmail.com>
2023-10-11 08:26:22 -0300

    FEAT: implementando middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import middleware from "./src/middlewares/middleware.js";
 const app = express();
 
 import routes from "./src/routes/index.js"
 const port = 3000;
 
 app.use(
-  express.json(), 
+  express.json(),
+  middleware, 
   routes
   );
 
